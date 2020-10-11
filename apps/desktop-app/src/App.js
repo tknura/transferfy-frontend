cb92cb70e7d32cf5e2692e1e876e15717c82699f
Tomasz Knura <tomasz.knura@itsilesia.com>
2020-10-11 22:27:47 +0200

    chore: add electron project with react (#3)

---
+++ apps/desktop-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Test
+      <div>
+        Test
+        Test
+      </div>
+      Test
+    </div>
+  );
+}
+
+export default App;
